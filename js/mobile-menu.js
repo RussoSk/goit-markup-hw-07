956b5dd124332fffd6eb53b5b3e66502310f8647
RussoSk <117772437+RussoSk@users.noreply.github.com>
2023-02-12 19:16:12 +0200

    Update mobile-menu.js

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -5,13 +5,19 @@
       closeMenuBtn: document.querySelector('[data-menu-close]'),
       menu: document.querySelector('[data-menu]'),
         body: document.querySelector('body'),
+        memuList:document.querySelector('.nav-mobile-menu'),
     };
   
     refs.openMenuBtn.addEventListener('click', toggleModal);
     refs.closeMenuBtn.addEventListener('click', toggleModal);
+    refs.memuList.addEventListener('click', removeMenu);
   
     function toggleModal() {
       refs.menu.classList.toggle('is-open');
       refs.body.classList.toggle('no-scroll');
+    }
+    function removeMenu() {
+      refs.menu.classList.add('is-open');
+      refs.body.classList.remove('no-scroll');
     }
   })();
