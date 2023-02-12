0ba38f37b2aa95edbeea13da1921cc552ad937d0
RussoSk <117772437+RussoSk@users.noreply.github.com>
2023-02-12 20:41:13 +0200

    1.8

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -4,20 +4,12 @@
       openMenuBtn: document.querySelector('[data-menu-open]'),
       closeMenuBtn: document.querySelector('[data-menu-close]'),
       menu: document.querySelector('[data-menu]'),
-        body: document.querySelector('body'),
-        memuList:document.querySelector('.mb-menu-list'),
     };
   
-    refs.openMenuBtn.addEventListener('click', toggleModal);
-    refs.closeMenuBtn.addEventListener('click', toggleModal);
-    refs.memuList.addEventListener('click', removeMenu);
+    refs.openMenuBtn.addEventListener('click', toggleMenu);
+    refs.closeMenuBtn.addEventListener('click', toggleMenu);
   
-    function toggleModal() {
-      refs.menu.classList.toggle('is-hidden');
-      refs.body.classList.toggle('no-scroll');
-    }
-    function removeMenu() {
-      refs.menu.classList.add('is-hidden');
-      refs.body.classList.remove('no-scroll');
+    function toggleMenu() {
+      refs.menu.classList.toggle('is-open');
     }
   })();
